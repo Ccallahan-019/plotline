2cb3cc44e4e447ce7cbb7432ee09dea5a1d2d5bd
Christian Callahan <callahan.0019@gmail.com>
2026-06-15 17:55:59 -0400

    Add ESLint and TypeScript configurations for Plotline monorepo

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import onlyWarn from "eslint-plugin-only-warn";
+import perfectionist from "eslint-plugin-perfectionist";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+
+/**
+ * Shared ESLint configuration for the Plotline monorepo.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export const config = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  perfectionist.configs["recommended-alphabetical"],
+  eslintConfigPrettier,
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    ignores: ["dist/**", ".next/**", "out/**", "build/**"],
+  },
+];
